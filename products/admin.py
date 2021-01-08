83e2dfdd0ff9247af45018d1ccb64473c745e706
Viacheslav <little.cado1@gmail.com>
2021-01-08 20:11:00 +0300

    Add models in products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categories, Products, Specification, Ratings, Reviews
+
+admin.site.register(Categories)
+admin.site.register(Products)
+admin.site.register(Specification)
+admin.site.register(Ratings)
+admin.site.register(Reviews)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.name
+
+class Specification(models.Model):
+    name = models.TextField('Product specification')
+
+    def __str__(self):
+        return self.name
+
+class Products(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.ForeignKey(Categories, on_delete=models.PROTECT)
+    specification = models.ForeignKey(Specification, on_delete=models.PROTECT)
+    link_to_photos = models.URLField(max_length=200)
+    short_description = models.TextField('Short description')
+
+    def __str__(self):
+        return self.name
+
+class Ratings(models.Model):
+    quality = models.IntegerField
+    functionality = models.IntegerField
+
+class Reviews(models.Model):
+    text = models.TextField('Review text')
+    recommendation = models.BooleanField
+    date = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Products, on_delete=models.PROTECT)
+    rating = models.ForeignKey(Ratings, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
