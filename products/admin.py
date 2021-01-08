ad9556df5659b29b06104f5c318dac5d2f05e30a
Viacheslav <little.cado1@gmail.com>
2021-01-08 23:26:11 +0300

    Add admin panel setup (filters, search)

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 
 from .models import Category, Product, Specification, Rating, Review
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'link_to_photos', 'short_description')
+    list_filter = ('category',)
+    search_fields = ('name', 'category__name')
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('quality', 'functionality')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('text', 'recommendation', 'date', 'product', 'rating')
+
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Specification)
-admin.site.register(Rating)
-admin.site.register(Review)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Review, ReviewAdmin)
